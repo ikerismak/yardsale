dccfa80b4b235c0d5712a09e934ee4f018983f0f
ikerismak <ikerismak@gmail.com>
2022-08-25 22:12:11 -0500

    toggle conditions added

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,7 @@ const mobileMenu = document.querySelector(".mobile-menu")
 
 const menuHam = document.querySelector(".menu")
 
-
-const shoppingCartIcon = document.querySelector(".navbar-shopping-cart");
+const shoppingCartIcon = document.querySelector(".shopping-cart-icon");
 
 const asideProductDetail = document.querySelector(".product-detail")
 
@@ -21,12 +20,30 @@ shoppingCartIcon.addEventListener("click",toggleShoppingCart);
 
 
 function toggleMenuDesktop(){
-   
-    menuDesktop.classList.toggle("inactive");
+
+    const isProductDetailClosed = asideProductDetail.classList.contains("inactive");
+
+    if(!isProductDetailClosed){
+
+        asideProductDetail.classList.add("inactive")
+    }
+
+    menuDesktop.classList.toggle("inactive")
+
+    console.log("que tranza");
 
 };
 
 function toggleMenuMobile(){
+
+    const isProductDetailClosed = asideProductDetail.classList.contains("inactive");
+
+    if(!isProductDetailClosed){
+
+       
+        asideProductDetail.classList.add("inactive")
+        
+    }
 
 
     mobileMenu.classList.toggle('inactive');
@@ -35,14 +52,16 @@ function toggleMenuMobile(){
 
 function toggleShoppingCart(){
 
-    const isMobileClosed = mobileMenu.classList.contains('inactive');
+    const  isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const  isDesktopMenuClosed = menuDesktop.classList.contains("inactive");
 
-    if(!isMobileClosed){
+    if(!isMobileMenuClosed  || !isDesktopMenuClosed){
 
-        
-        mobileMenu.classList.toggle('inactive');
+        menuDesktop.classList.add("inactive")
 
+        mobileMenu.classList.add("inactive")
     }
-
     asideProductDetail.classList.toggle('inactive');
+
+
 }
